9cb94ea5bce2878bb434da4614a613d102568d38
Joohoon Lee <joohoonlee@Harrys-MacBook-Pro-5.local>
2022-11-28 22:51:56 -0500

    add metamask login

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,44 @@
 import React, { useState } from "react";
 import BaseLayout from "../components/Layout";
+import { ethers } from "ethers";
+
+
 
 const MainPage = () => {
+  
+  async function metamaskLogin() {
+      // A Web3Provider wraps a standard Web3 provider, which is
+      // what MetaMask injects as window.ethereum into each page
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+    // MetaMask requires requesting permission to connect users accounts
+    await provider.send("eth_requestAccounts", []);
+
+    // The MetaMask plugin also allows signing transactions to
+    // send ether and pay to change state within the blockchain.
+    // For this, you need the account signer...
+    const signer = provider.getSigner()
+  }
+
+
   return (
     <BaseLayout>
-      <div class="flex items-center justify-center w-full">
-        <h1>Hello World!</h1>
-      </div>
+      <nav className="bg-white border-gray-200 px-2 md:px-4 py-2.5 dark:bg-gray-900">
+          <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+              <a href="https://flowbite.com" className="flex items-center">
+                  <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo"/>
+                  <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Crowd Label</span>
+              </a>
+              <div className="flex items-center md:order-2">
+                  <a href="#" onClick={metamaskLogin} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"> Login with Wallet</a>
+                  <button data-collapse-toggle="mega-menu" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                   aria-controls="mega-menu" aria-expanded="false">
+                      <span className="sr-only">Login with metamask</span>
+                      <svg aria-hidden="true" className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+                  </button>
+              </div>
+          </div>
+      </nav>
     </BaseLayout>
   );
 };
