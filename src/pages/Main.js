70098ab326a2bc9a34ea37db30f7bfa70f0a0da1
John Hojae Yoon <53275319+hojaedev@users.noreply.github.com>
2022-12-02 21:22:28 -0500

    modify main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
-import BaseLayout from "../components/Layout";
+import {BaseLayout} from "../components/Layout";
 import { ethers } from "ethers";
 
-
-
 const MainPage = () => {
-
-
   return (
     <BaseLayout>
+      <div className="bg-black flex h-screen items-center justify-between px-28">
+        <h1 className="flex-1 w-100 text-white text-5xl font-extrabold">
+          Powering the Next Generation ML Tasks: Decentralized Distributed
+          Labeling Platform
+        </h1>
+        <img src="./main.svg" alt="3d" className="flex-none" />
+      </div>
     </BaseLayout>
   );
 };
